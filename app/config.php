d564691cc0a2ec3951c2bb2a58fe88f5c26b20fb
Ken Depelchin <ken.depelchin@gmail.com>
2013-06-09 17:41:08 +0200

    add last.fm api keys to config service provider

--- app/config.php
+++ app/config.php
@@ -14,5 +14,9 @@ return array(
         'consumer_key' => 'YxVwmymcXeQEkcXxdqZv',
         'consumer_secret' => 'cIPHAdkoChFCgCRdrkCuRVYNbONZljbo'
     ),
+    'api.lastfm' => array(
+        'consumer_key' => 'c56c47c9e3a2424421590e0b19cf2c29',
+        'consumer_secret' => '4cbe3e28b5621a4b1a1293f833e34090',
+    ),
     'salt' => 'salt'
 );
